be696ddb05388ebe2a4691416b9f11bc81d10489
Brayan Acosta Vivas <bacostav@ufpso.edu.co>
2024-03-08 21:14:29 -0500

    clase 2

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -1,11 +1,32 @@
-export const getProducto = (req,res)=>{
-    res.status(200).json({msg:"esto es GET"})
+import { getProductoModel, getProductoUnicoModel, PostProductoModel,UpdateProductoModel } from "../models/producto.model.js"
+
+export const getProducto =async  (req,res)=>{
+    let data =await  getProductoModel();
+    res.status(200).json({msg:"esto es GET", data})
 }
-export const postProducto = (req,res)=>{
-    res.status(200).json({msg:"esto es POST"})
+
+export const getProductoUnico=async(req,res)=>{
+    const {id}=req.params;
+    let data = await getProductoUnicoModel(id);
+    res.status(200).json({msg:"esto es GET unico", data})
+}
+
+export const postProducto = async (req,res)=>{
+    const {detalle,nombre,valor} = req.body;
+    let data = await PostProductoModel(detalle,nombre,valor);
+    res.status(200).json({msg:"esto es POST", data})
+}
+
+export const updateProducto = async (req,res)=>{
+    const {id}=req.params;
+    const {detalle,nombre,valor} = req.body;
+    let data = await UpdateProductoModel(detalle,nombre,valor,id);
+    res.status(200).json({msg:"esto es POST", data})
 }
 
 export default {
     getProducto,
-    postProducto
+    getProductoUnico,
+    postProducto,
+    updateProducto
 }
--- routes/producto.routes.js
+++ routes/producto.routes.js
@@ -4,7 +4,9 @@ import productoController from "../controllers/producto.controller.js";
 const routerProducto= Router();
 
 routerProducto.get("/",productoController.getProducto);
+routerProducto.get("/:id",productoController.getProductoUnico);
 routerProducto.post("/",productoController.postProducto);
+routerProducto.put("/:id", productoController.updateProducto);
 
 
 export default routerProducto;
