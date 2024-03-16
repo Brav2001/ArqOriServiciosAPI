e16e5fb6551def085fa5b18353f35ffbf3f59730
Brayan Acosta Vivas <bacostav@ufpso.edu.co>
2024-03-15 20:52:04 -0500

    clase 3

--- config/sever.js
+++ config/sever.js
@@ -3,6 +3,7 @@ import express from "express";
 import { env } from "./default.js";
 import route from "../routes/index.routes.js";
 import pgService from "../services/pg.service.js";
+import middleware from "../middleware/index.middlewares.js";
 
 export default class Server{
     constructor(){
@@ -14,9 +15,10 @@ export default class Server{
     }
     middlewares(){
         this.app.use(express.json());
+        this.app.use(middleware);
     }
     routes(){
-        this.app.use(route)
+        this.app.use(route);
     }
     runServer(){
         this.app.listen(this.port, ()=>{
--- routes/producto.routes.js
+++ routes/producto.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import productoController from "../controllers/producto.controller.js";
+import { validate } from "../middleware/validator.middleware.js";
+import { postProductoValidator } from "../validators/producto.validator.js";
 
 const routerProducto= Router();
 
 routerProducto.get("/",productoController.getProducto);
 routerProducto.get("/:id",productoController.getProductoUnico);
-routerProducto.post("/",productoController.postProducto);
+routerProducto.post("/", validate(postProductoValidator),productoController.postProducto);
 routerProducto.put("/:id", productoController.updateProducto);
 
 
